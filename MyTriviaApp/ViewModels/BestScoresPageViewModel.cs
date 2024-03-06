cea3a5d6d634f4f46ea89ac745efda3a9b7aa748
User <User@LAB3-3>
2024-03-06 08:18:54 +0200

    e

--- MyTriviaApp/ViewModels/BestScoresPageViewModel.cs
+++ MyTriviaApp/ViewModels/BestScoresPageViewModel.cs
@@ -5,11 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using MyTriviaApp.Services;
 using MyTriviaApp.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MyTriviaApp.ViewModels
 {
-    public class BestScoresPageViewModel:ViewModel
+    public class BestScoresPageViewModel : ViewModel
     {
+        public ObservableCollection<Player> Players { get; set; }
+        public ICommand LoadPlayersCommand { get; private set; }
+        public ICommand RefreshCommand { get; private set; }
+        private bool isRefreshing;
+        public bool IsRefreshing { get => isRefreshing; set { isRefreshing = value; OnPropertyChanged(); } }
+        private Service service;
+        public BestScoresPageViewModel(Service service)
+        {
+            this.service = service;
 
+            Players = new ObservableCollection<Player>();
+            LoadPlayersCommand = new Command(async () => await LoadPlayers());
+            RefreshCommand = new Command(async () => await Refresh());
+        }
+
+        private async Task LoadPlayers()
+        {
+            var fullList = await service.GetStudents();
+            Players.Clear();
+            foreach (var player in fullList)
+            {
+                Players.Add(player);
+            }
+        }
+        private async Task Refresh()
+        {
+            IsRefreshing = true;
+            await LoadPlayers();
+            IsRefreshing = false;
+        }
     }
 }
