e2ed0bbaf237c0f7551002db0d57ede523f56cc9
User <User@LAB3-3>
2024-03-06 08:20:10 +0200

    e

--- MyTriviaApp/AppShell.xaml.cs
+++ MyTriviaApp/AppShell.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace MyTriviaApp
+﻿using MyTriviaApp.Views;
+
+namespace MyTriviaApp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+        }
+        private void RoutingPages()
+        {
+            Routing.RegisterRoute("BestScores", typeof(BestScoresPage));
+            Routing.RegisterRoute("Login", typeof(LoginPage));
+
         }
     }
 }
