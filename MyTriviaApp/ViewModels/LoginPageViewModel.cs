bb469738f53af8d859d72582d9dc4e62f7bcc242
User <User@LAB3-3>
2024-03-06 08:22:32 +0200

    wh

--- MyTriviaApp/ViewModels/LoginPageViewModel.cs
+++ MyTriviaApp/ViewModels/LoginPageViewModel.cs
@@ -8,27 +8,28 @@ using MyTriviaApp.Services;
 
 namespace MyTriviaApp.ViewModels
 {
-    public class LoginPageViewModel:ViewModel
+    public class LoginPageViewModel : ViewModel
     {
-        
+
         private string username;
         private string password;
-        public string UserName { get{ return username; } set { username = value; OnPropertyChanged(); ((Command)LoginCommand).ChangeCanExecute(); } }
-        public string Password { get { return password; } set { password = value;OnPropertyChanged();((Command)LoginCommand).ChangeCanExecute(); } }
-        public ICommand LoginCommand {  get; set; }
-        public ICommand CancelCommand {  get; set; }
-        private Color color;
+        public string UserName { get { return username; } set { username = value; OnPropertyChanged(); ((Command)LoginCommand).ChangeCanExecute(); } }
+        public string Password { get { return password; } set { password = value; OnPropertyChanged(); ((Command)LoginCommand).ChangeCanExecute(); } }
+        public ICommand LoginCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+        private Service service;
 
-        public LoginPageViewModel() 
+        public LoginPageViewModel(Service service)
         {
             LoginCommand = new Command(Login, () => { return !string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password); });
+            this.service = service;
         }
 
         private void Login()
         {
-           Service x=new Service();
-            bool result=x.Login(UserName, Password);
-          
+
+            bool result = service.Login(UserName, Password);
+
         }
     }
 }
